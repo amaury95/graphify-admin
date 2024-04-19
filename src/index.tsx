bd127f971509d583e4c765fe96aa4c2f0ca0ecbd
Amaury Diaz <amauryuh@gmail.com>
2024-04-20 00:20:19 +0200

    add some hooks to fetch data

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "index.css";
 import reportWebVitals from "reportWebVitals";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 import { AuthProvider } from "provider/Auth";
 import { baseUrl } from "api/baseUrl";
 import { Layout } from "app/layouts/Layout";
@@ -37,6 +43,7 @@ function Router() {
     </HashRouter>
   );
 }
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
